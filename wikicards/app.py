55d822eaf096caee32c856d217ecdea96b35588a
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-11-17 11:09:17 -0300

    Add scaffold for chemical compounds

--- wikicards/app.py
+++ wikicards/app.py
@@ -89,6 +89,49 @@ def particular_cell(cell_qid):
     return web_page
 
 
+@app.route("/compound", methods=["GET", "POST"])
+def compound():
+    if request.method == "POST":
+        compound = request.form["compound"]
+        return redirect(f"/compound/{compound}")
+    all_compounds = get_all_compounds()
+    return render_template("public/compound.html", compounds=all_compounds)
+
+
+@app.route("/compound/<compound_qid>", methods=["GET", "POST"])
+def particular_compound(compound_qid):
+
+    compound_template = Template(
+        QUERIES.joinpath("compound_template.rq.jinja").read_text(encoding="UTF-8")
+    )
+    query = compound_template.render(target=compound_qid)
+
+    try:
+        wikidata_result = query_wikidata(query)[0]
+    except IndexError:
+        all_compounds = get_all_compounds()
+
+        return render_template(
+            "public/compound.html",
+            compounds=all_compounds,
+            compound_not_found_message=f'<div class="alert alert-warning" role="alert">No results found", try another.</div>',
+        )
+    ids = extract_ids(compound_qid, wikidata_result)
+
+    summaries = {}
+    summaries["wikipedia"] = get_wikipedia_summary(wikidata_result["en_wiki_label"])
+
+    web_page = render_template(
+        "public/compound.html",
+        wikidata_result=wikidata_result,
+        ids=ids,
+        summaries=summaries,
+    )
+
+    web_page = clean_up_pmids(web_page)
+    return web_page
+
+
 @app.route("/disease", methods=["GET", "POST"])
 def disease():
     if request.method == "POST":
@@ -285,6 +328,24 @@ def get_all_diseases():
         "?item rdfs:label ?itemLabel . "
         "FILTER (LANG (?itemLabel) = 'en') }"
     )
+    all_diseases = get_all_of_a_kind_for_jinja(query)
+    return all_diseases
+
+
+def get_all_compounds():
+
+    query = (
+        "SELECT DISTINCT ?itemLabel"
+        '  (REPLACE(STR(?item), ".*Q", "Q") AS ?qid) '
+        " WHERE {?item wdt:P31 wd:Q113681859 .  "
+        "?item rdfs:label ?itemLabel . "
+        "FILTER (LANG (?itemLabel) = 'en') }"
+    )
+    all_compounds = get_all_of_a_kind_for_jinja(query)
+    return all_compounds
+
+
+def get_all_of_a_kind_for_jinja(query):
     all_diseases_df = wikidata2df(query)
     all_diseases = []
     for _, row in all_diseases_df.iterrows():
